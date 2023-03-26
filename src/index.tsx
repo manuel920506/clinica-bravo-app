ec1736fd99d634ab7581607aac3e43dbba2c45d3
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2023-03-26 07:45:05 +0100

    tmp implementing i18next for translate

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/flag-icon-css/css/flag-icons.min.css'; 
+import i18next from 'i18next'; 
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import './index.css';
+import App from './App'; 
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'es', 'it'],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  })
 
 ReactDOM.render(
   <React.StrictMode>
