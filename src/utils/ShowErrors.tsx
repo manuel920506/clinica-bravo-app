cc03029d4e07a0dc9355ba8c5607e62f7eb57561
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2023-04-25 23:27:46 +0200

    resolving errors inside ShowErrors

--- src/utils/ShowErrors.tsx
+++ src/utils/ShowErrors.tsx
@@ -1,56 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ShowErrors(props: showErrorsProps){
     const style= {color: 'red'};
-    const [errors, setErrors] = useState<any>(props);
-
-    console.log('ci sono errori: ', props.errors);
-
+    const [errors, setErrors] = useState<errorData[]>([]);   
+    let arr : errorData[] = [];
+    
+    console.log('ci sono errori: ', props.errors); 
     console.log('del tipo: ', typeof props.errors); 
 
     switch(typeof props.errors) { 
         case 'string': { 
-            console.log(props.errors); 
+            let error : errorData = {code : "" , description : props.errors}
+            arr.push(error);
+            console.log(props.errors);  
            break; 
         } 
-        case 'object': { 
-            var objectError: any = props.errors
-            setErrors(objectError.errors);
+        case 'object': {  
+            let objectError: any = props.errors; 
             if( objectError.length){ 
                 objectError.forEach((error: errorData) => { 
-                    console.log(error.description);
+                    console.log(error.description); 
+                    arr.push(error);
                   }); 
             }
             
-            if(objectError.errors){  
+            if(objectError.errors){   
                 for (var error in objectError.errors){
                     console.log(error + ': ' + objectError.errors[error]);
+                    let _error : errorData = {code : error, description :objectError.errors[error]};
+                    arr.push(_error);
                }  
-            }
+            } 
            break; 
         } 
-        default: { 
-            var _errors : errorData[] = props.errors;
-            setErrors(objectError.errors);
+        default: {   
+            let _errors : errorData[] = props.errors;  
             _errors.forEach((error: errorData) => { 
                 console.log(error.description);
-              }); 
+                arr.push(error);
+            });  
            break; 
         } 
-     }  
+    };
+    
+    useEffect(()=>{
+		if(arr.length > 0){ 
+            setErrors(arr);
+        }
+	}, [arr]);
     return(
         <> 
-            {props.errors ? <ul style={style}>
-                { 
-                 typeof props.errors === 'string' ? <li>{props.errors}</li> : 
-                 typeof props.errors === 'object' && errors ? errors.map((error : any, index: number) => <li key={index}>{errors[error]}</li>)  :
-                 typeof props.errors === 'object' ? null :
-                 errors.map((error :errorData, index: number) => <li key={index}>{error.description}</li>)}
-            </ul> : null} 
-        {/* 
-            {props.errors ? <ul style={style}>
-                { typeof props.errors === 'string' ? <li>{props.errors}</li> : props.errors.map((error, index) => <li key={index}>{error.description}</li>)}
-            </ul> : null} */}
+            {errors ? <ul style={style}>
+                { errors.map((error :errorData, index: number) => <li key={index}>{error.description}</li>)}
+            </ul> : null}  
         </>
     )
 }
